baa9ab368e7655ac4791d14024e967c3f842dbd8
Mfiocco <fiocco@hawaii.edu>
2025-05-07 11:18:47 -1000

    swapped out v3 with io version

--- js/parallax.js
+++ js/parallax.js
@@ -1,42 +1,17 @@
-// PARALLAX
-function parallax({ el, ease = "linear" }) {
-
-    let scrolling = false;
-
-    function runParallax() {
-        const pageTop = window.pageYOffset;
-        const pageMid = pageTop + window.innerHeight / 2;
-        const topSection = el.offsetTop;
-        const midSection = topSection + el.offsetHeight / 2;
-        const viewDistanceLeft = pageMid - midSection;
-        const parallaxSpeed = parseFloat(el.getAttribute("data-parallax-speed"));
-
-        // Set the parallax movement
-        el.style.transform = `translate3d(0, ${(viewDistanceLeft * parallaxSpeed) / 3}px, 0)`;
-        el.style.transition = `transform ${ease}`;
-    }
-
-    window.addEventListener("scroll", () => {
-        if (!scrolling) {
-            window.requestAnimationFrame(() => {
-                runParallax();
-                scrolling = false;
-            });
+$(document).ready(function () {
+  
+    //init scrolling parallax
+    $(window).scroll(function(e){
+        var scrolled = $(window).scrollTop();
+        if(scrolled < 750){
+            parallax()
         }
-        scrolling = true;
     });
-
-    runParallax();
-
-}
-
-// Parallax
-// const parallaxElements = document.querySelectorAll(".parallax-onscroll");
-// parallaxElements.forEach((el) => parallax({ el }));
-
-// IF WINDOW SIZE IS GREATER THE 768px  
-if (window.innerWidth > 768) {
-    // Parallax
-    const parallaxElements = document.querySelectorAll(".parallax-onscroll");
-    parallaxElements.forEach((el) => parallax({ el }));
-}
+  
+    //define parallax function
+    function parallax(){
+      var scrolled = $(window).scrollTop();
+      $('#parallax').css('background-positionY',(scrolled * -0.5)+'px');
+    };
+    
+  });
